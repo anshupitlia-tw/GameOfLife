e51228e2b54519ad686010ca83b11d5a9cfa0b5a
anshup <anshup@INanshup.local>
2015-09-19 12:17:48 +0530

    A game of life universe should be able to find the no of live cells in the eight neighbourhood cell of a cell

--- src/com/twp/gameoflife/GameOfLifeUniverse.java
+++ src/com/twp/gameoflife/GameOfLifeUniverse.java
@@ -5,12 +5,21 @@ import java.util.ArrayList;
 
 public class GameOfLifeUniverse {
     private ArrayList<GameOfLifeGridRows> columns;
+    private static int MIN_INDEX = 0;
+    private static int MAX_INDEX;
 
     GameOfLifeUniverse(ArrayList<GameOfLifeGridRows> columns) {
         this.columns = columns;
+        this.MAX_INDEX = columns.size();
     }
 
     public int findNumberOfLiveCells(int rowIndex, int columnIndex) {
-        return 5;
+        int count = 0;
+        count += columns.get(rowIndex).findNumberOfLiveCellsToTheLeftAndRight(columnIndex);
+        if (rowIndex > MIN_INDEX)
+            count += columns.get(rowIndex - 1).findNumberOfLiveCellsToTheLeftAndRightIncludingSelf(columnIndex);
+        if (rowIndex < MAX_INDEX)
+            count += columns.get(rowIndex + 1).findNumberOfLiveCellsToTheLeftAndRightIncludingSelf(columnIndex);
+        return count;
     }
 }
--- test/com/twp/gameoflife/GameOfLifeUniverseTest.java
+++ test/com/twp/gameoflife/GameOfLifeUniverseTest.java
@@ -46,4 +46,43 @@ public class GameOfLifeUniverseTest {
 
         assertEquals(5, gameOfLifeUniverse.findNumberOfLiveCells(1, 1));
     }
+
+    @Test
+    public void shouldFindNumberOfLiveCellsInTheEightNeighboursOfACellAndReturnFourIfFourCellsAreLive() {
+        ArrayList<StateOfCell> rowOne = new ArrayList<>();
+        StateOfCell cellOne = new LiveCell();
+        StateOfCell cellTwo = new LiveCell();
+        StateOfCell cellThree = new DeadCell();
+        rowOne.add(cellOne);
+        rowOne.add(cellTwo);
+        rowOne.add(cellThree);
+        GameOfLifeGridRows gameOfLifeGridRowsOne = new GameOfLifeGridRows(rowOne);
+
+        ArrayList<StateOfCell> rowTwo = new ArrayList<>();
+        cellOne = new LiveCell();
+        cellTwo = new DeadCell();
+        cellThree = new DeadCell();
+        rowTwo.add(cellOne);
+        rowTwo.add(cellTwo);
+        rowTwo.add(cellThree);
+        GameOfLifeGridRows gameOfLifeGridRowsTwo = new GameOfLifeGridRows(rowTwo);
+
+        ArrayList<StateOfCell> rowThree = new ArrayList<>();
+        cellOne = new DeadCell();
+        cellTwo = new LiveCell();
+        cellThree = new DeadCell();
+        rowThree.add(cellOne);
+        rowThree.add(cellTwo);
+        rowThree.add(cellThree);
+        GameOfLifeGridRows gameOfLifeGridRowsThree = new GameOfLifeGridRows(rowTwo);
+
+        ArrayList<GameOfLifeGridRows> gameOfLifeGridRowses = new ArrayList<>();
+        gameOfLifeGridRowses.add(gameOfLifeGridRowsOne);
+        gameOfLifeGridRowses.add(gameOfLifeGridRowsTwo);
+        gameOfLifeGridRowses.add(gameOfLifeGridRowsThree);
+
+        GameOfLifeUniverse gameOfLifeUniverse = new GameOfLifeUniverse(gameOfLifeGridRowses);
+
+        assertEquals(4, gameOfLifeUniverse.findNumberOfLiveCells(1, 1));
+    }
 }
